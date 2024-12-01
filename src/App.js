b24be77e87c7d30671662597d57302c9eb338bf0
LHoRrrr <lihorcuteboy007@gmail.com>
2024-12-01 11:38:59 +0700

    Create Footer And Header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home'
 import About from './pages/About'
@@ -6,23 +5,27 @@ import News from './pages/News'
 import Shop from './pages/Shop'
 import Contact from './pages/Contact'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Header from "./pages/Header"
+import Footer from "./pages/Footer"
 
 
 
 function App() {
   return (
    <>
-   <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home></Home>}/>
-        <Route path='/about' element={<About></About>}/>
-        <Route path='/news' element={<News></News>}/>
-        <Route path='/contact' element={<Contact></Contact>}/>
-        <Route path='/shop' element={<Shop></Shop>}/>
-      </Routes>
-   </BrowserRouter>
+    <Header></Header>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home></Home>}/>
+          <Route path='/about' element={<About></About>}/>
+          <Route path='/news' element={<News></News>}/>
+          <Route path='/contact' element={<Contact></Contact>}/>
+          <Route path='/shop' element={<Shop></Shop>}/>
+        </Routes>
+    </BrowserRouter>
+    <Footer></Footer>
    </>
+   
   );
 }
-
 export default App;
