234847a7dea075fa8c22ef42d3f941a62696bccf
LHoRrrr <lihorcuteboy007@gmail.com>
2024-12-01 12:40:12 +0700

    create page cart, checkout, contact, error, news

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Contact from './pages/Contact'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from "./pages/Header"
 import Footer from "./pages/Footer"
+import Cart from "./pages/Cart"
+import Checkout from "./pages/Checkout"
+import Error from "./pages/Error"
+import SingleNews from "./pages/SingleNews"
+
 
 
 
@@ -20,6 +25,10 @@ function App() {
           <Route path='/about' element={<About></About>}/>
           <Route path='/news' element={<News></News>}/>
           <Route path='/contact' element={<Contact></Contact>}/>
+          <Route path='/cart' element={<Cart></Cart>}/>
+          <Route path='/checkout' element={<Checkout></Checkout>}/>
+          <Route path='/single news' elemlent={<SingleNews></SingleNews>}/>
+          <Route path='/error' element={<Error></Error>}/>
           <Route path='/shop' element={<Shop></Shop>}/>
         </Routes>
     </BrowserRouter>
